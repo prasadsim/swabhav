bfa6a5e74aef48cd9b42d2948375ad6741dfec39
prasad <prasadsim075@gmail.com>
2020-05-04 19:41:43 +0530

    04/05/2020

--- OOP/urldataextraction-app/src/com/techlabs/model/test/Test.java
+++ OOP/urldataextraction-app/src/com/techlabs/model/test/Test.java
@@ -2,6 +2,7 @@ package com.techlabs.model.test;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Collection;
 import java.util.TreeSet;
 import com.techlabs.model.*;
 
@@ -10,7 +11,7 @@ public class Test {
 	public static void main(String[] args) throws ParseException, IOException {
 		GetFromUrl u = new GetFromUrl();
 		u.getUrlContents("https://swabhav-tech.firebaseapp.com/emp.txt");
-		TreeSet<Employee> tree = (TreeSet<Employee>) u.getEmployees();
+		Collection<Employee> tree = u.getEmployees();
 		for (Employee employee : tree) {
 			System.out.println("Id:" + employee.getEid() + "\tSalary:" + employee.getSalary());
 		}
