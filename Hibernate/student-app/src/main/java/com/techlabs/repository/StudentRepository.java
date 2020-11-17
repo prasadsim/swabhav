60bf65b22b441652c25cfc7ca15c01c03d5258af
prasad <prasadsim075@gmail.com>
2020-11-17 21:08:51 +0530

    17/11/2020

--- Hibernate/student-app/src/main/java/com/techlabs/repository/StudentRepository.java
+++ Hibernate/student-app/src/main/java/com/techlabs/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.techlabs.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Repository;
 import com.techlabs.entity.Student;
 
 @Repository
+@Transactional
 public class StudentRepository {
 
 	private List<Student> students = null;
@@ -17,34 +20,34 @@ public class StudentRepository {
 	private SessionFactory factory;
 
 	public List<Student> getStudents() {
-		Session session = factory.openSession();
-		students = session.createQuery("from Student").list();
+		students = factory.getCurrentSession().createQuery("from Student").list();
 		return students;
 	}
 
 	public void addStudent(Student s) {
-		Session session = factory.openSession();
-		session.save(s);
+		factory.getCurrentSession().save(s);
 	}
 
 	public void updateStudent(Student student) {
-		Session session = factory.openSession();
-		Student s = (Student) session.load(Student.class, student.getId());
+		Student s = getStudentById(student.getId());
 		s.setName(student.getName());
 		s.setAge(student.getAge());
 		s.setRollno(student.getRollno());
-		session.update(s);
+		factory.getCurrentSession().update(s);
 	}
 
 	public void deleteStudent(String id) {
-		Session session = factory.openSession();
-		Student s = (Student) session.load(Student.class, id);
-		session.delete(s);
+		Student s = getStudentById(id);
+		factory.getCurrentSession().delete(s);
 	}
 
 	public Student getStudentById(String id) {
-		Session session = factory.openSession();
-		Student s = (Student) session.load(Student.class, id);
-		return s;
+		getStudents();
+		for (Student s : students) {
+			if (s.getId().equals(id)) {
+				return s;
+			}
+		}
+		return null;
 	}
 }
